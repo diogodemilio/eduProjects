1b94595b018acab8fd50f8dfa60a486ce8a7c488
diogodemilio <20781172+diogodemilio@users.noreply.github.com>
2021-03-08 10:35:52 +0000

    demy update

---
+++ Udemy/JavascriptFundamentals/modal/script.js
@@ -0,0 +1,26 @@
+"use strict";
+
+const modal = document.querySelector(".modal");
+const overlay = document.querySelector(".overlay");
+const btnCloseModal = document.querySelector(".close-modal");
+const btnShowModal = document.querySelectorAll(".show-modal");
+
+const removeHidden = () => {
+	modal.classList.remove("hidden");
+	overlay.classList.remove("hidden");
+};
+
+for (let index = 0; index < btnShowModal.length; index += 1) {
+	btnShowModal[index].addEventListener("click", removeHidden);
+}
+
+const addHidden = () => {
+	modal.classList.add("hidden");
+	overlay.classList.add("hidden");
+};
+
+btnCloseModal.addEventListener("click", addHidden);
+
+overlay.addEventListener("click", addHidden);
+
+document.addEventListener("keydown", function () {});
