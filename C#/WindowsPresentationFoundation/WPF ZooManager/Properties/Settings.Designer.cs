4cda7a6428b4deb8b555a46b2539af9a2f66ad63
AbstractEdge <d.emilio@campus.fct.unl.pt>
2021-09-05 17:55:45 +0100

    WPF and Databases learning followUp

--- C#/WindowsPresentationFoundation/WPF ZooManager/Properties/Settings.Designer.cs
+++ C#/WindowsPresentationFoundation/WPF ZooManager/Properties/Settings.Designer.cs
@@ -12,7 +12,7 @@ namespace WPF_Zoo_Manager.Properties {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "17.0.2.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "16.10.0.0")]
     internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
         
         private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
@@ -31,6 +31,17 @@ namespace WPF_Zoo_Manager.Properties {
         public string CSharpTutorialsDBConnectionString {
             get {
                 return ((string)(this["CSharpTutorialsDBConnectionString"]));
+            }
+        }
+        
+        [global::System.Configuration.ApplicationScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.SpecialSettingAttribute(global::System.Configuration.SpecialSetting.ConnectionString)]
+        [global::System.Configuration.DefaultSettingValueAttribute("Data Source=ABSTRACTEDGE-LE;Initial Catalog=CSharpTutorialDB;Persist Security Inf" +
+            "o=True;User ID=sa;Password=Diogo-4231!")]
+        public string CSharpTutorialDBConnectionString {
+            get {
+                return ((string)(this["CSharpTutorialDBConnectionString"]));
             }
         }
     }
